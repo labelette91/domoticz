c63245afc95b6b1b316502548b1c935569a65207
pte <pte@>
2019-08-29 19:32:25 +0200

    add enocean controler

--- www/app/hardware/HardwareSetup.js
+++ www/app/hardware/HardwareSetup.js
@@ -7,6 +7,7 @@ define([
     'hardware/setup/Pinger',
     'hardware/setup/WakeOnLan',
     'hardware/setup/ZWave',
+    'hardware/setup/EnOcean',
 ], function (app) {
     app.controller('HardwareSetupController', function ($routeParams, domoticzApi) {
         var vm = this;
